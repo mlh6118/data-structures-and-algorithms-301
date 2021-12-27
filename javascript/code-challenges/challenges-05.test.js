9ab73df47d27d469290037ba03d7db5e8cf1c2ad
Marni Hager <mlh6118@gmail.com>
2021-12-27 13:29:16 -0800

    Completed challenge 4.

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -66,7 +66,7 @@ Note: You may not use the array's built-in length property.
 ------------------------------------------------------------------------------------------------ */
 
 const countNumberOfElements = (arr) => {
-  // Solution code here...
+  return arr.reduce((sum, arr) => sum + 1, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
