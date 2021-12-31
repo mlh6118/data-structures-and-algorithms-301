9fe9920af58d8c4a431cf4a89b5ded14cbfad926
Marni Hager <mlh6118@gmail.com>
2021-12-31 00:16:04 -0800

    Completed challenge 6.

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -96,7 +96,6 @@ console.log(people[1].isAuthor) prints true
 const setStatusAsAuthor = (people) => {
   // array.forEach(objectInArray => objectInArray.variableToAdd = value)
   people.forEach(element => element.isAuthor = true);
-  // console.log(people);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -115,8 +114,8 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
-
+  // secondArray.forEach(bananas => firstArray.push(bananas));
+  arr2.forEach(element => arr1.push(element));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -176,7 +175,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
