ea83d3ca131cb085b4d7ebcdb9ecc4a2b7ae736f
Marni Hager <mlh6118@gmail.com>
2022-01-30 16:36:09 -0800

    Completed code challenge 4.

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -56,7 +56,7 @@ For example, (123) 456-7890 returns 1234567890
 ------------------------------------------------------------------------------------------------ */
 
 const standardizePhoneNumbers = (arr) => {
-  // Solution code here...
+  return arr.map(string => string.substring(1, 4) + string.substring(6, 9) + string.substring(10));
 };
 
 /* ------------------------------------------------------------------------------------------------
