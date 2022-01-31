69dfbf0658f851bf982915842d2fc8588d78174c
Marni Hager <mlh6118@gmail.com>
2022-01-30 16:32:35 -0800

    Completed code challenge 3.

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -44,7 +44,7 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this
 ------------------------------------------------------------------------------------------------ */
 
 const findHappiness = (arr) => {
-  // Solution code here...
+  return arr.filter(string => string.includes(':)'));
 };
 
 /* ------------------------------------------------------------------------------------------------
